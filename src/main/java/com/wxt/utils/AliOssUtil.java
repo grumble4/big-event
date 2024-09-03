536555c70e3b25a164ab6fe3b9698fea73223ba0
grumble4 <146970239+grumble4@users.noreply.github.com>
2024-09-03 22:39:05 +0800

    Update AliOssUtil.java
    账号，密码隐藏了，使用自己的

--- src/main/java/com/wxt/utils/AliOssUtil.java
+++ src/main/java/com/wxt/utils/AliOssUtil.java
@@ -15,8 +15,8 @@ public class AliOssUtil {
     private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
     //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private static final String ACCESS_KEY_ID="LTAI5t64suzy2sVPuKMRDATo";
-    private static final String ACCESS_KEY_SECRET="OP5bLZy4EUXn4MRFIGT9Ppgkr1sCn6";
+    private static final String ACCESS_KEY_ID="使用自己的";
+    private static final String ACCESS_KEY_SECRET="使用自己的";
     // 填写Bucket名称，例如examplebucket。
     private static final String BUCKET_NAME = "big-event-pro-w";
     public static String uploadFile(String objectName, InputStream in) throws Exception {
